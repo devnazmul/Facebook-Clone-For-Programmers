e5f065b00e72ca7ae4195eb3e49886479f91300f
Md Nazmul Islam <md.nazmul.islam1332@google.com>
2021-08-29 22:59:18 +0600

    add .env file and clean code in server.js

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require('express');
+
 const app = express();
-const mongoose = require('mongoose');
+
+//Config DotENV 
+require('dotenv').config();
 
 //DB Config
-const db = require('./config/keys').mongoURI;
-mongoose.connect(db)
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI)
         .then(console.log(`Database is connected......`))
         .catch(err => console.log(err));
 
+//Routers
+
+
 //Listening to port
-let PORT = 5000;
-app.listen(PORT,()=>{
+let port = process.env.PORT || 5000;
+let host = process.env.HOST || 'localhost';
+app.listen(port,host,()=>{
     console.log(`Server is running.......`);
-    console.log(`Visit: http://localhost:${PORT}`);
+    console.log(`Visit: http://localhost:${port}`);
 });
