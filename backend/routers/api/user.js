d60979ed5fc606ac21041ea0a9f7bde7ef9648e3
Md Nazmul Islam <md.nazmul.islam1332@google.com>
2021-09-05 00:12:23 +0600

    User registration functionality is done!

---
+++ backend/routers/api/user.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const gravatar = require('gravatar');
+const bcrypt = require('bcrypt');
+
+const userRouter = express.Router();
+
+const User = require('../../models/User');
+const { json } = require('body-parser');
+
+
+/*  
+    @ Route   :   User Registration
+    @ Desc    :   User Registration
+    @Security :   Public
+*/
+userRouter.post('/register', (req, res) => {
+  User.findOne({ email: req.body.email }).exec()
+    .then((user) => {
+      if (user) {
+        res.status(400).json({ message: 'Already have an account with this email!' })
+      } else {
+        bcrypt.hash(req.body.email, 10).then(hash => {
+          const newUser = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: hash,
+            avatar: gravatar.url(req.body.email, {s: '100', r: 'x', d: 'retro'}, true)
+          })
+          newUser.save().then(() => {
+            res.status(200).json({ message: 'User Resistration Successfully....' })
+          }).catch(() => {
+            res.status(400)
+          })
+        })
+          .catch((err) => console.log(err))
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+
+module.exports = userRouter;
--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,30 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
+//Init Express App
 const app = express();
 
 //Config DotENV 
 require('dotenv').config();
 
+//Using Middlewares
+app.use(express.json());
+
 //DB Config
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect('mongodb://127.0.0.1:27017')
         .then(console.log(`Database is connected......`))
         .catch(err => console.log(err));
 
-//Routers
 
+//Routers
+const userRouter = require('./routers/api/user');
+app.use('/routers/api/user', userRouter);
+ 
 
 //Listening to port
-let port = process.env.PORT || 5000;
+let port = process.env.PORT || 3000;
 let host = process.env.HOST || 'localhost';
-app.listen(port,host,()=>{
+app.listen(port,()=>{ 
     console.log(`Server is running.......`);
-    console.log(`Visit: http://localhost:${port}`);
+    console.log(`Visit: http://${host}:${port}`); 
 });
