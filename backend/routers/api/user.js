98083058074ee458ef97185862c7423e7a7f678a
Md Nazmul Islam <md.nazmul.islam1332@google.com>
2021-09-05 17:37:44 +0600

    User Login Functionality And JWT Token Genaration

--- backend/routers/api/user.js
+++ backend/routers/api/user.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const gravatar = require('gravatar');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-const userRouter = express.Router();
+const router = express.Router();
 
 const User = require('../../models/User');
 
@@ -11,32 +12,63 @@ const User = require('../../models/User');
     @ desc    User Registration
     @access   Public
 */
-userRouter.post('/register', (req, res) => {
-  User.findOne({ email: req.body.email }).exec()
-    .then((user) => {
-      if (user) {
-        res.status(400).json({ message: 'Already have an account with this email!' })
-      } else {
-        bcrypt.hash(req.body.email, 10).then(hash => {
-          const newUser = new User({
-            name: req.body.name,
-            email: req.body.email,
-            password: hash,
-            avatar: gravatar.url(req.body.email, {s: '100', r: 'x', d: 'retro'}, true)
-          })
-          newUser.save().then(() => {
-            res.status(200).json({ message: 'User Resistration Successfully....' })
-          }).catch(() => {
-            res.status(400)
-          })
-        })
-          .catch((err) => console.log(err))
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+router.post('/register', async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email }).exec()
+    if (user) {
+      res.status(400).json({ 'message': 'Already have an account with this email!' })
+    } else {
+      const hasedPassword = await bcrypt.hash(req.body.password, 10);
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: hasedPassword,
+        avatar: gravatar.url(req.body.email, { s: '100', r: 'x', d: 'retro' }, true)
+      })
+      newUser.save().then(
+        res.status(200).json({ 'message': 'User Resistration Successfully....' })
+      )
+    }
+  } catch (error) {
+    res.status(500).json(error)
+  }
+
+})
+
+
+/*  
+    @ route   /routers/api/user/login
+    @ desc    User Login
+    @access   Public
+*/
+router.post('/login', async (req, res) => {
+  //Find User Is Available Or Not
+  const user = await User.findOne({ email: req.body.email });
+
+  if (user) { // If User Founded.
+    // Compare Password Id Authenticated Or Not.
+    const isAuthenticated = await bcrypt.compare(req.body.password, user.password);
+    if (isAuthenticated) { // If User Is Authenticated.
+      //Creating User JWT
+      let token = jwt.sign({
+        userId: user._id,
+        userName: user.name
+      }, process.env.JWT_SECRET, {
+        expiresIn: '2 days'
+      });
+
+      res.status(200).json({ // Sending Token
+        'token' : token,
+        'message': 'Authentication Done!' 
+      });
+
+    } else { // If User Is Unauthenticated.
+      res.status(401).json({ 'message': 'Authentication Failed! .' });
+    }
+
+  } else { // If User Not Found.
+    res.status(401).json({ 'message': 'Authentication Failed! .' });
+  }
 });
 
-
-module.exports = userRouter;
+module.exports = router;
